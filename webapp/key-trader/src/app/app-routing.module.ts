dd026396ac64ff2534706d13ce679341eceb963e
Allison Thompson <allisonthompson1996@gmail.com>
2020-11-14 19:01:50 -0800

    add LoginFormComponent app-routing/module.ts

--- webapp/key-trader/src/app/app-routing.module.ts
+++ webapp/key-trader/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {LoginPageComponent} from './components/login-page/login-page.component';
+import {LoginFormComponent} from './components/login-form/login-form.component';
 import {ServersPageComponent} from './components/servers-page/servers-page.component';
 import {AddKeyPageComponent} from './components/add-key-page/add-key-page.component';
 import {SeeKeysPageComponent} from './components/see-keys-page/see-keys-page.component';
@@ -15,6 +16,7 @@ import {ProfilePageComponent} from './components/profile-page/profile-page.compo
 const routes: Routes = [
   {path: '', component: ServersPageComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginPageComponent},
+  {path: 'login-form', component: LoginFormComponent},
   {path: 'add-key', component: AddKeyPageComponent, canActivate: [NormalAuthGuard]},
   {path: 'see-keys', component: SeeKeysPageComponent, canActivate: [NormalAuthGuard]},
   {path: 'view-roles', component: ViewRolesComponent, canActivate: [NormalAuthGuard]},
