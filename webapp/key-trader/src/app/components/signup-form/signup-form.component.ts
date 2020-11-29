5c22622ac6dbdc57e78580d3e4afd3f28bb8dcbc
Allison Thompson <allisonthompson1996@gmail.com>
2020-11-29 15:19:28 -0800

    added post request signup-form

--- webapp/key-trader/src/app/components/signup-form/signup-form.component.ts
+++ webapp/key-trader/src/app/components/signup-form/signup-form.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../../Auth/auth.service';
+import { AuthService } from '../../Auth/auth.service' ;
+import {NgForm} from '@angular/forms';
+import { AuthData } from '../../Models/auth-data.model';
 
 @Component({
   selector: 'app-signup-form',
   templateUrl: './signup-form.component.html',
-  styleUrls: ['./signup-form.component.css']
+  styleUrls: ['./signup-form.component.css'],
+  providers: [AuthService]
 })
 export class SignupFormComponent implements OnInit {
 
-  constructor(private router: Router,
-              private authService: AuthService) { }
+  constructor( private router: Router,
+    private userService: AuthService) { }
 
   password: String;
   confirm: String;
@@ -27,12 +30,15 @@ export class SignupFormComponent implements OnInit {
     this.password = form.value.passwordInput;
     this.confirm= form.value.confirmInput;
 
-    if (this.password != this.confirm) {
+    if (form.invalid) {
+      console.log("Invalid form.")
+    }
+    else if (this.password != this.confirm) {
       console.log("Passwords do not match.")
     }
     else {
       console.log(form.value);
-      //this.userService.fetchUser();
+      this.userService.createUser(form.value.email, form.value.password);
     }
   }
 
