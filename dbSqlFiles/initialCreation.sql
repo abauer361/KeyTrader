d5d98f6d9a1a039a8896744898d1ea200122ce43
Allison Thompson <allisonthompson1996@gmail.com>
2020-11-29 20:13:57 -0800

    added KeyTraderUser create table statement initialCreation.sql

--- dbSqlFiles/initialCreation.sql
+++ dbSqlFiles/initialCreation.sql
@@ -98,6 +98,17 @@ INSERT INTO `User_Role` VALUES (1,1,'Admin'),(2,2,'Donor/Recipient'),(3,3,'Donor
 /*!40000 ALTER TABLE `User_Role` ENABLE KEYS */;
 UNLOCK TABLES;
 
+DROP TABLE IF EXISTS `KeyTraderUsers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `KeyTraderUsers` (
+  `Email` varchar(50) DEFAULT NULL,
+  `Token` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
 
 DROP TABLE IF EXISTS `Users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
