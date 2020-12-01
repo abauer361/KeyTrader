e8fe83ee57affc088d3a79b1ad4b1289d044ff8f
Allison Thompson <allisonthompson1996@gmail.com>
2020-12-01 00:04:35 -0800

    completed bcrypt hash for authentication user.js

--- webapp/backend/backend/routes/user.js
+++ webapp/backend/backend/routes/user.js
@@ -3,31 +3,60 @@ const bcrypt = require('bcrypt');
 const router = express.Router();
 
 const databaseRecords = require('../db');
+const { BadRequestError } = require('../errorUtil');
 
-router.post("/signup", (req, res, next) => {
+router.post("/login", async (req, res, next) => {
+    //get user
+
+    //define attributes
+    const email = req.body.email;
+
+    //pull from database
+    try {
+        const results = await databaseRecords.getKeyTraderUsers(email);
+        const keyJson = JSON.parse(JSON.stringify(results));
+        
+        var match = false;
+        //we used the primary key, so array should be size 1
+        for (const user in keyJson) {
+          var check = bcrypt.compareSync(req.body.password,keyJson[user].Token);
+          if (match == false) {
+            match = check;
+          }
+        }
+        return res.status(200).json({token:match});
+      }
+      catch (err) {
+        return next(new BadRequestError("Failed to get user authorization", err));
+      }
+});
+
+router.post("/signup", async (req, res, next) => {
     
     //create user
+
+    //hash password
     const saltRounds = 10;
     const salt = bcrypt.genSaltSync(saltRounds);
-    
     const hash = bcrypt.hashSync(req.body.password, salt);
-    const authData = {
-        email: req.body.email,
-        token: hash
-    };
+
+    //define attributes
     const email = req.body.email;
     const token = hash;
+
     //insert to database
     try {
-        databaseRecords.createKeyTraderUser(email, token, function(){
-            res.redirect('/');
-          },function(err){
-              return res.status(500).json(err);
-          });
-        return res.status(201).json({msg:"success"});
+        await databaseRecords.createKeyTraderUser(email, token);
+        return res.status(201).json({
+            msg:"Signup Success",
+            result: { 
+                email: email,
+                token: token
+                }
+            });
     }
     catch (err) {
-      return next(new BadRequest("Failed to create user", err));
+      return next(new BadRequestError("Failed to create user", err));
     }
 });
 
