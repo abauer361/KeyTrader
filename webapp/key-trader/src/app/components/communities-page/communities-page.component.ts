49fe0058209b34e3f5fb78ea3c7c3093aee7ceb6
Allison Thompson <allisonthompson1996@gmail.com>
2020-12-07 20:19:36 -0800

    communities dummy page with login only communities-page

--- webapp/key-trader/src/app/components/communities-page/communities-page.component.ts
+++ webapp/key-trader/src/app/components/communities-page/communities-page.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {AuthService} from '../../Auth/auth.service';
 import { Router } from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-communities-page',
   templateUrl: './communities-page.component.html',
   styleUrls: ['./communities-page.component.css']
 })
-export class CommunitiesComponent  {
+export class CommunitiesComponent implements OnInit {
 
+  isLoading = false;
+  userAuthenticated = false;
   constructor(private router: Router,
     public authService: AuthService) { }
 
   ngOnInit(): void {
-      
+      this.isLoading = true;
+      this.userAuthenticated = this.authService
+      .getIsAuthenticated();
   }
+
+  
 
 
 }
