3d3a343c9e72a686d97ec9a8540d87cfd6efb4c8
Allison Thompson <allisonthompson1996@gmail.com>
2020-12-07 20:21:49 -0800

    added nav menu communities-nav-menu

---
+++ webapp/key-trader/src/app/components/communities-nav-menu/communities-nav-menu.component.spec.ts
@@ -0,0 +1,81 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { CommunitiesNavMenuComponent } from './nav-menu.component';
+
+describe('CommunitiesNavMenuComponent', () => {
+  let component: CommunitiesNavMenuComponent;
+  let fixture: ComponentFixture<CommunitiesNavMenuComponent>;
+  let debugElement: DebugElement;
+  let router: Router;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CommunitiesNavMenuComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    router = TestBed.get(Router);
+    fixture = TestBed.createComponent(CommunitiesNavMenuComponent);
+    component = fixture.componentInstance;
+    debugElement = fixture.debugElement.query(By.css('button'));
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should display 'Home'`, () => {
+    // Assert the text on screen says "Home"
+    const btn = fixture.debugElement.nativeElement.querySelector('#home');
+    expect(btn.innerHTML).toEqual('Home');
+  });
+
+  it(`should display 'Profile'`, () => {
+    // Assert the text on screen says "Profile"
+    const btn = fixture.debugElement.nativeElement.querySelector('#profile');
+    expect(btn.innerHTML).toEqual('Profile');
+  });
+
+  it(`should display 'Logout'`, () => {
+    // Assert the text on screen says "Logout"
+    const btn = fixture.debugElement.nativeElement.querySelector('#logout');
+    expect(btn.innerHTML).toEqual('Logout');
+  });
+
+  it(`should navigate to homepage`, () => {
+    // Assert the router should navigate to Home
+    const navigateSpy = spyOn(router, 'navigate');
+    const btn = fixture.debugElement.nativeElement.querySelector('#home');
+    btn.click();
+    expect(navigateSpy).toHaveBeenCalledWith(['/']);
+  });
+
+  it(`should navigate to profile page`, () => {
+    // Assert the router should navigate to Home
+    const navigateSpy = spyOn(router, 'navigate');
+    const btn = fixture.debugElement.nativeElement.querySelector('#profile');
+    btn.click();
+    expect(navigateSpy).toHaveBeenCalledWith(['/profile-page']);
+  });
+
+  it(`should logout`, () => {
+    // Assert the router should navigate to Home
+    const navigateSpy = spyOn(router, 'navigate');
+    const btn = fixture.debugElement.nativeElement.querySelector('#logout');
+    btn.click();
+    expect(navigateSpy).toHaveBeenCalledWith(['/login']);
+  });
+});
---
+++ webapp/key-trader/src/app/components/communities-nav-menu/communities-nav-menu.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../Auth/auth.service';
+
+@Component({
+  selector: 'app-communities-nav-menu',
+  templateUrl: './communities-nav-menu.component.html',
+  styleUrls: ['./communities-nav-menu.component.css']
+})
+export class CommunitiesNavMenuComponent implements OnInit {
+
+  constructor(private router: Router,
+              private authService: AuthService) { }
+
+  ngOnInit(): void { }
+
+  onHomeButtonClicked(): void {
+    console.log('Going to home page');
+    this.router.navigate(['/']);
+  }
+
+  onProfileButtonClicked(): void {
+    console.log('Going to profile page');
+    this.router.navigate(['/profile-page']);
+  }
+
+  onLogoutButtonClicked(): void {
+    console.log('Logging Out');
+    this.authService.logoutKeyTraderUser();
+  }
+
+}
